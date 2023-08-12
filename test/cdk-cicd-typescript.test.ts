b34fd76ff790d893882498f37cf0f9baa6c29f29
Rafik Mondal <70977282+rafik790@users.noreply.github.com>
2023-08-13 02:20:38 +0530

    Added test stage with test pipeline

--- test/cdk-cicd-typescript.test.ts
+++ test/cdk-cicd-typescript.test.ts
@@ -1,3 +1,9 @@
-test('Init Test', () => {
+import { handler } from "../services/hello-lambda";
 
-});
+describe('Hello describe test suite', ()=>{
+
+    test('handler should return 200',async ()=>{
+        const result = await handler({}, {})
+        expect(result.statusCode).toBe(200);
+    })
+})
